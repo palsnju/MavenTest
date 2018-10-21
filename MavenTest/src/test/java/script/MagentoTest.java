cfde1a0dfc77c82ad697b7b87a50c9b060e68fbe
Sanjay <Sanjay@DESKTOP-8M20IUQ>
2018-10-21 21:51:53 +0530

    1st time

---
+++ MavenTest/src/test/java/script/MagentoTest.java
@@ -0,0 +1,38 @@
+package script;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import objectRepository.Login;
+import objectRepository.Logout;
+import objectRepository.Main;
+
+
+
+
+
+public class MagentoTest {
+@Test
+	public void posCredentials() {
+		
+		WebDriver d=new ChromeDriver();
+		d.manage().window().maximize();
+		d.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		d.get("https://www.magento.com");
+		Main m=new Main(d);
+		m.clickOnMyacc();
+		Login l=new Login(d);
+		l.typeEmail("subramanyaraj87@gmail.com");
+		l.typePass("Welcome123");
+		l.clickOnLogin();
+		Logout lo=new Logout(d);
+		lo.clickOnLogout();
+		d.quit();
+		
+
+	}
+
+}
